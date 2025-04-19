2df4cab9323890d2cf2bf24d0866689d569bcedb
SuperMoloko <158835564+SuperMoloko@users.noreply.github.com>
2025-04-19 18:31:22 +0300

    Initial commit

---
+++ db_config.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$dbname = "sms";
+$username= "root";
+$password = "root";
+$port = 3306;
+
+
+
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port",$username,$password);
+
+
+
+
+
+?>
