9871794855ed7579cf5be6f33cf83490c1d569d8
SuperMoloko <158835564+SuperMoloko@users.noreply.github.com>
2025-04-26 09:27:37 +0300

    1
    1

--- index.php
+++ index.php
@@ -10,7 +10,62 @@
 </head>
 
 <body>
+    <header>
+        <div class="logo">
+            <img src="img/logo.svg" alt="">
+        </div>
+    </header>
+    <main>
+        <form action="" id="newtask">
+            <input id="newrequest" type="text" placeholder="type a new task here">
+            <div class="submitbtn">
+                <p>Add</p>
+                <img src="img/add.svg" alt="">
+            </div>
+            
+        </form>
 
+        <div id="statistics">
+            <div class="task-counter">
+                <p>All tasks</p>
+                <p class="counter">0</p>
+            </div>
+            <div class="task-done">
+                <p>Done tasks</p>
+                <p class="counter">0</p>
+            </div>
+        
+        </div>
+
+        <div class="empty">
+            <div class="line">
+
+            </div>
+            <div class="empty-tasks">
+                <img src="img/Clipboard.svg" alt="">
+                <h2>There is no actually tasks</h2>
+                <p>Click on button to add</p>
+            </div>
+        </div>
+
+            <!-- <div class="new-tasks">
+                <div class="task">
+                    <div class="checkbox">
+                        <input type="checkbox" name="" id="">
+                    </div>
+                    <div class="task-description">
+                    Integer urn sometimes mass free author nor ugly ugly always. Duis or but hunger intact.
+                    </div>
+                    <div class="urna">
+                        <img src="img/urna.svg" alt="">
+                    </div>
+
+                </div>
+            </div> -->
+
+
+        <hr>
+    </main>
     
 </body>
 
